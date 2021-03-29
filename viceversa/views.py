cbeed6f5f0f8ba1afc85fe54653bc697b1e3e6b0
EL Studente <stasi1357@mail.ru>
2021-03-29 21:23:40 +0300

    deem it was hard to make this true i still dont undestend some features but at least it bette than before

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+
+
 def home(request):
 	return render(request, 'home.html')
+
+
+def reverse(request):
+	user_text = request.GET['usertext']
+	words = user_text.split()
+	words2 = 'ура, сучары'
+	#print(words2)
+	number_of_words = len(words)
+	reversed_text = user_text[::-1]
+	# ключ из reverse.html. Значения - переменные из функции
+	return render(request, 'reverse.html', {'usertext':user_text, 
+		'reversedtext':reversed_text, 'number_of_words':number_of_words, 'words22': words2}, )
